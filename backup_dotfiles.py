814f3f3d37dae701837ba77c1a66ac91287c16d9
Brandon Morimoto <bmorimoto99@gmail.com>
2021-12-19 12:42:23 -0800

    created backup script

---
+++ backup_dotfiles.py
@@ -0,0 +1,22 @@
+from os import chdir, mkdir
+from os.path import expanduser, exists, isfile
+from time import time
+from shutil import copy2, copytree
+
+chdir(expanduser('~'))
+files_to_copy = ['.config','.bashrc','.bash_profile','.gitconfig','.gitignore_global',
+                '.aliases','.hushlogin','.tmux.conf','.vimrc','.antigen.zsh','.p10k.zsh','.zshrc']
+
+backup_name = "dotfile-backup-" + str(time())
+mkdir(backup_name)
+
+print('backing up dotfiles...')
+for file in files_to_copy:
+    if not exists(file):
+        continue
+    elif isfile(file):
+        copy2(file, backup_name)
+    else:
+        copytree(file, backup_name + '/' + file)
+
+print('dotfiles backed up to', backup_name)
