38059a2b4444a75df8b61459df8334327339b180
Brandon Morimoto <bmorimoto99@gmail.com>
2022-02-13 04:28:12 -0800

    added jump_to_last autocommand

--- nvim/lua/user/autocommands.lua
+++ nvim/lua/user/autocommands.lua
@@ -3,4 +3,11 @@ vim.cmd [[
         autocmd!
         au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
     augroup END
+
+    augroup jump_last_location
+        if has("autocmd")
+            au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$")
+            \| exe "normal! g'\"" | endif
+        endif
+    augroup END
 ]]
