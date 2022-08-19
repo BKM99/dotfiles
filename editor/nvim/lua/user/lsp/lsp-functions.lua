b63bf7b03f10fa04c429ae14ed85debfd6963185
Brandon Morimoto <bmorimoto99@gmail.com>
2022-08-18 19:28:51 -0700

    fixed deprecated method for formatting

--- editor/nvim/lua/user/lsp/lsp-functions.lua
+++ editor/nvim/lua/user/lsp/lsp-functions.lua
@@ -41,7 +41,7 @@ local function lsp_keymaps(bufnr)
     keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
     keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
     keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format { async = true}<CR>", opts)
 end
 
 local function lspsaga_keymaps(bufnr)
