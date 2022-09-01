99c4c8b94c717ef427ddbba439eae2818bff3dda
Brandon Morimoto <bmorimoto99@gmail.com>
2022-09-01 02:41:02 -0700

    updated package script

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,10 +1,9 @@
 # download homebrew if it is not installed
 if ! command -v brew &> /dev/null
 then
-    echo "homebrew could not be found"
-    echo "installing homebrew..."
+    echo "homebrew could not be found. Installing homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "done installing brew!"
+    echo "done installing homebrew!"
 fi
 
 echo "installing brew packages..."
@@ -14,16 +13,21 @@ brew install tmux
 brew install git
 brew install lazygit
 brew install lazydocker
-brew install --HEAD neovim
+brew install neovim
 brew install autojump
 brew install exa
 brew install go
 brew install gradle
 brew install yarn
+brew install tree
 brew install ripgrep
 brew install fzf
-brew install docker
+brew install fd
 brew install btop
+brew install rustup
+rustup-init -y
+
+brew install --cask docker
 brew install --cask amethyst
 brew install --cask kitty
 brew install --cask zoom
@@ -31,16 +35,17 @@ brew install --cask spotify
 brew install --cask discord
 brew install --cask the-unarchiver
 brew install --cask google-chrome
+
 brew install --cask miniconda
 conda init "$(basename "${SHELL}")"
+
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
 brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-noto-nerd-font
+brew install --cask font-fira-mono-nerd-font
 
 echo "done installing brew packages"
-
-echo "installing rust..."
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
 echo "installing sdkman..."
 curl -s "https://get.sdkman.io" | bash
