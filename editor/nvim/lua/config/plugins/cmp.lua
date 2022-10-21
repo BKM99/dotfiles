f95773fcb9bec5d7e8306c3368c3cc75168797b7
Brandon Morimoto <bmorimoto99@gmail.com>
2022-10-20 22:40:22 -0700

    cmp updates

--- editor/nvim/lua/config/plugins/cmp.lua
+++ editor/nvim/lua/config/plugins/cmp.lua
@@ -13,14 +13,7 @@ if not lsp_kind_status_ok then
 	return
 end
 
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-require("luasnip/loaders/from_vscode").lazy_load({
-	path = "~/.local/share/nvim/site/pack/packer/start/friendly-snippets/snippets",
-})
+require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
 	snippet = {
@@ -30,43 +23,39 @@ cmp.setup({
 	},
 
 	mapping = cmp.mapping.preset.insert({
-		["<S-TAB>"] = cmp.mapping.select_prev_item(),
-		["<TAB>"] = cmp.mapping.select_next_item(),
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-		["<Tab>"] = cmp.mapping(function(fallback)
+		-- this is from https://github.com/abecodes/tabout.nvim README.md
+		["<Tab>"] = function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
+				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback)
+		end,
+		["<S-Tab>"] = function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
+				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
+		end,
 	}),
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
 		{ name = "nvim_lua" },
+		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "path" },
 	}, {
-		{ name = "buffer" },
+		{ name = "buffer", keyword_length = 5 },
 	}),
 	confirm_opts = {
 		behavior = cmp.ConfirmBehavior.Replace,
@@ -116,6 +105,7 @@ cmp.setup.cmdline(":", {
 })
 
 -- Database completion
-vim.api.nvim_exec([[
-autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
-]], false)
+vim.api.nvim_exec(
+	[[ autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} }) ]],
+	false
+)
