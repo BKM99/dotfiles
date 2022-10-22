aca477029e07ef6f099b6cf32208cc61319222c8
Brandon Morimoto <bmorimoto99@gmail.com>
2022-10-22 12:12:48 -0700

    more gitsigns keymaps

--- editor/nvim/lua/config/plugins/gitsigns.lua
+++ editor/nvim/lua/config/plugins/gitsigns.lua
@@ -6,8 +6,13 @@ end
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
-keymap("n", "[h", ":Gitsigns next_hunk<CR>", opts)
-keymap("n", "]h", ":Gitsigns prev_hunk<CR>", opts)
+keymap("n", "]h", ":Gitsigns next_hunk<CR>", opts)
+keymap("n", "[h", ":Gitsigns prev_hunk<CR>", opts)
 keymap("n", "<leader>bl", ":Gitsigns blame_line<CR>", opts)
+keymap("n", "<leader>hs", ":Gitsigns stage_hunk<CR>", opts)
+keymap("v", "<leader>hs", ":Gitsigns stage_hunk<CR>", opts)
+keymap("n", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>", opts)
+keymap("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", opts)
+keymap("v", "<leader>hr", ":Gitsigns reset_hunk<CR>", opts)
 
 gitsigns.setup()
