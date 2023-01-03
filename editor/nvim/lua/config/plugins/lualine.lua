a1e93a6c5022cb6f3c98dade96ec12dd3e75502f
Brandon Morimoto <bmorimoto99@gmail.com>
2023-01-03 01:47:27 -0800

    lualine updates

--- editor/nvim/lua/config/plugins/lualine.lua
+++ editor/nvim/lua/config/plugins/lualine.lua
@@ -9,10 +9,7 @@ local config = {
         theme = "catppuccin",
         -- component_separators = { left = "|", right = "|" },
         -- section_separators = { left = "", right = "" },
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
+        disabled_filetypes = { "NvimTree" },
         ignore_focus = {},
         always_divide_middle = true,
         globalstatus = false,
