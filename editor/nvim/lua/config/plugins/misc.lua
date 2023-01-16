c74e622eff38220efa321de130d6f48ffc42313a
Brandon Morimoto <bmorimoto99@gmail.com>
2023-01-16 13:19:06 -0800

    moved trouble

--- editor/nvim/lua/config/plugins/misc.lua
+++ editor/nvim/lua/config/plugins/misc.lua
@@ -33,4 +33,22 @@ return {
 			vim.keymap.set("n", "<leader>sr", "<cmd>lua require('spectre').open()<CR>")
 		end,
 	},
+	{
+		"folke/trouble.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+
+		config = function()
+			local trouble = require("trouble")
+			local opts = { noremap = true, silent = true }
+			local keymap = vim.keymap.set
+
+			keymap("n", "<leader>e", ":TroubleToggle<CR>", opts)
+
+			trouble.setup({
+				use_diagnostic_signs = true,
+			})
+		end,
+	},
 }
