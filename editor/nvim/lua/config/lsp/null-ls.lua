2d4266b81e9f408bf5fafc874c084daef7a8c4bb
Brandon Morimoto <bmorimoto99@gmail.com>
2023-01-16 06:24:21 -0800

    formatting

--- editor/nvim/lua/config/lsp/null-ls.lua
+++ editor/nvim/lua/config/lsp/null-ls.lua
@@ -4,15 +4,15 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-    debug = false,
-    sources = {
-        formatting.prettierd.with({
-            extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-        }),
-        formatting.black.with({ extra_args = { "--fast" } }),
-        diagnostics.flake8,
-        formatting.stylua,
-        formatting.goimports,
-        formatting.gofumpt,
-    },
+	debug = false,
+	sources = {
+		formatting.prettierd.with({
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		diagnostics.flake8,
+		formatting.stylua,
+		formatting.goimports,
+		formatting.gofumpt,
+	},
 })
--- editor/nvim/lua/config/lsp/setup.lua
+++ editor/nvim/lua/config/lsp/setup.lua
@@ -5,141 +5,141 @@ local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
 mason.setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-        },
-    },
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 local servers = {
-    "jsonls",
-    "sumneko_lua",
-    "clangd",
-    "tsserver",
-    "gopls",
-    "html",
-    "pyright",
-    "rust_analyzer",
-    "tailwindcss",
-    "yamlls",
-    "cssls",
-    "eslint",
-    "jdtls",
-    "elixirls",
-    "angularls",
+	"jsonls",
+	"sumneko_lua",
+	"clangd",
+	"tsserver",
+	"gopls",
+	"html",
+	"pyright",
+	"rust_analyzer",
+	"tailwindcss",
+	"yamlls",
+	"cssls",
+	"eslint",
+	"jdtls",
+	"elixirls",
+	"angularls",
 }
 
 mason_lspconfig.setup({
-    ensure_installed = servers,
+	ensure_installed = servers,
 })
 
 local tools = {
-    "js-debug-adapter",
-    "delve",
-    "black",
-    "flake8",
-    "prettierd",
-    "stylua",
-    "codelldb",
-    "java-debug-adapter",
-    "java-test",
-    "gofumpt",
-    "goimports",
+	"js-debug-adapter",
+	"delve",
+	"black",
+	"flake8",
+	"prettierd",
+	"stylua",
+	"codelldb",
+	"java-debug-adapter",
+	"java-test",
+	"gofumpt",
+	"goimports",
 }
 
 -- Install tools
 local mr = require("mason-registry")
 for _, tool in ipairs(tools) do
-    local p = mr.get_package(tool)
-    if not p:is_installed() then
-        p:install()
-    end
+	local p = mr.get_package(tool)
+	if not p:is_installed() then
+		p:install()
+	end
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-    opts = {
-        on_attach = require("config.lsp.handlers").on_attach,
-        capabilities = require("config.lsp.handlers").capabilities,
-    }
+	opts = {
+		on_attach = require("config.lsp.handlers").on_attach,
+		capabilities = require("config.lsp.handlers").capabilities,
+	}
 
-    if server == "sumneko_lua" then
-        local sumneko_opts = {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                    workspace = {
-                        library = {
-                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                            [vim.fn.stdpath("config") .. "/lua"] = true,
-                        },
-                    },
-                },
-            },
-        }
-        opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-    end
+	if server == "sumneko_lua" then
+		local sumneko_opts = {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		}
+		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	end
 
-    -- if server == "pyright" then
-    --     local pyright_opts = {
-    --         settings = {
-    --             python = {
-    --                 analysis = {
-    --                     typeCheckingMode = "basic",
-    --                     diagnosticMode = "workspace",
-    --                     inlayHints = {
-    --                         variableTypes = true,
-    --                         functionReturnTypes = true,
-    --                     },
-    --                 },
-    --             },
-    --         },
-    --     }
-    --     opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-    -- end
+	-- if server == "pyright" then
+	--     local pyright_opts = {
+	--         settings = {
+	--             python = {
+	--                 analysis = {
+	--                     typeCheckingMode = "basic",
+	--                     diagnosticMode = "workspace",
+	--                     inlayHints = {
+	--                         variableTypes = true,
+	--                         functionReturnTypes = true,
+	--                     },
+	--                 },
+	--             },
+	--         },
+	--     }
+	--     opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	-- end
 
-    if server == "jsonls" then
-        local jsonls_opts = {
-            settings = {
-                json = {
-                    schemas = require("schemastore").json.schemas(),
-                },
-            },
-        }
-        opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-    end
+	if server == "jsonls" then
+		local jsonls_opts = {
+			settings = {
+				json = {
+					schemas = require("schemastore").json.schemas(),
+				},
+			},
+		}
+		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+	end
 
-    if server == "tailwindcss" then
-        local tailwindcss_opts = {
-            root_dir = require("lspconfig").util.root_pattern("tailwind.config.js"),
-        }
-        opts = vim.tbl_deep_extend("force", tailwindcss_opts, opts)
-    end
+	if server == "tailwindcss" then
+		local tailwindcss_opts = {
+			root_dir = require("lspconfig").util.root_pattern("tailwind.config.js"),
+		}
+		opts = vim.tbl_deep_extend("force", tailwindcss_opts, opts)
+	end
 
-    if server == "jdtls" then
-        goto continue
-    end
+	if server == "jdtls" then
+		goto continue
+	end
 
-    if server == "elixirls" then
-        local elixirls_opts = {
-            cmd = { os.getenv("HOME") .. "/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
-            settings = {
-                elixirLS = {
-                    dialyzerEnabled = false,
-                    fetchDeps = false,
-                },
-            },
-        }
+	if server == "elixirls" then
+		local elixirls_opts = {
+			cmd = { os.getenv("HOME") .. "/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
+			settings = {
+				elixirLS = {
+					dialyzerEnabled = false,
+					fetchDeps = false,
+				},
+			},
+		}
 
-        opts = vim.tbl_deep_extend("force", elixirls_opts, opts)
-    end
+		opts = vim.tbl_deep_extend("force", elixirls_opts, opts)
+	end
 
-    lspconfig[server].setup(opts)
-    ::continue::
+	lspconfig[server].setup(opts)
+	::continue::
 end
